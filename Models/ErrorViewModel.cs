9a1d75ce9d51b33ae394dda3e7b476c9c2d20100
mkhoatd <binnhatle@gmail.com>
2022-10-18 10:14:36 +0700

    abc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _102190334_NguyenMinhQuang.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
